312dd0dff9bf343404f7848c0a8845a43ed1902e
CHN-STUDENT <CHN-STUDENT@outlook.com>
2017-12-27 20:13:02 +0800

    upload

---
+++ Project1/cglmtest.cpp
@@ -0,0 +1,14 @@
+/*cglmtest.cpp*/
+#include <stdio.h>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+//#include "shader_c.h"
+#include "stb_image.h"
+#include <cglm\cglm.h>
+#define STB_IMAGE_IMPLEMENTATION
+
+int main(void)
+{
+
+	return 0;
+}
