d90ec7d84b7495ff80ab9a26ea646e992565a1ef
CHN-STUDENT <CHN-STUDENT@outlook.com>
2018-01-07 10:17:37 +0800

    继续学习

--- test.cpp
+++ test.cpp
@@ -9,10 +9,30 @@
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);//鼠标反馈
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);//滚轮反馈
 
 // settings
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
+
+// camera
+vec3 cameraPos = {0.0f, 0.0f, 3.0f};
+vec3 cameraFront = {0.0f, 0.0f, -1.0f};
+vec3 cameraUp = {0.0f, 1.0f, 0.0f};
+
+// timing
+float deltaTime = 0.0f;	// time between current frame and last frame
+float lastFrame = 0.0f;
+
+//mouse
+bool firstMouse = true;
+float yaw = -90.0f;	// yaw is initialized to -90.0 degrees since a yaw of 0.0 results in a direction vector pointing to the right so we initially rotate a bit to the left.
+float pitch = 0.0f;
+float lastX = 800.0f / 2.0;
+float lastY = 600.0 / 2.0;
+float fov = 45.0f;
+
 int main()
 {
 	// glfw: initialize and configure
@@ -37,6 +57,8 @@ int main()
 	}
 	glfwMakeContextCurrent(window);
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	glfwSetCursorPosCallback(window, mouse_callback);
+	glfwSetScrollCallback(window, scroll_callback);
 
 	// glad: load all OpenGL function pointers
 	// ---------------------------------------
@@ -204,6 +226,9 @@ int main()
 	// -----------
 	while (!glfwWindowShouldClose(window))
 	{
+		float currentFrame = glfwGetTime();
+		deltaTime = currentFrame - lastFrame;
+		lastFrame = currentFrame;
 		// input
 		// -----
 		processInput(window);
@@ -221,24 +246,28 @@ int main()
 
 		use();
 		mat4 view = GLM_MAT4_IDENTITY_INIT;//观察矩阵
-		float radius = 10.f;
-		float camX = sin(glfwGetTime()) * radius;
-		float camZ = cos(glfwGetTime()) * radius;
-		vec3 e = { camX,0.0f,camZ };//摄像机位置
-		vec3 c = { 0.0f,0.0f,0.0f };//摄像机方向
-		vec3 u = { 0.0f, 1.0f, 0.0f };//右轴？
-		glm_lookat(e, c, u, view);
-		//mat4 model=GLM_MAT4_IDENTITY_INIT;//模型矩阵
-		//vec3 m = { 0.5f, 1.0f, 0.0f };
-		//glm_rotate(model, (float)glfwGetTime(),m );
-		vec3 v = { 0.0f, 0.0f, -3.0f };
-		glm_translate(view, v);
-		mat4 projection = GLM_MAT4_IDENTITY_INIT;//投影矩阵
-		glm_perspective(glm_rad(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f,projection);
+		//float radius = 10.f;
+		//float camX = sin(glfwGetTime()) * radius;
+		//float camZ = cos(glfwGetTime()) * radius;
+		//vec3 e = { camX,0.0f,camZ };//摄像机位置
+		//vec3 c = { 0.0f,0.0f,0.0f };//摄像机方向
+		//vec3 u = { 0.0f, 1.0f, 0.0f };//右轴？
+		vec3 add;
+		glm_vec_add(cameraPos, cameraFront, add);
+		glm_lookat(cameraPos, add, cameraUp, view);
 		unsigned int viewLoc = glGetUniformLocation(ID, "view");
+		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, view[0]);
+		////mat4 model=GLM_MAT4_IDENTITY_INIT;//模型矩阵
+		////vec3 m = { 0.5f, 1.0f, 0.0f };
+		////glm_rotate(model, (float)glfwGetTime(),m );
+		//vec3 v = { 0.0f, 0.0f, -3.0f };
+		//glm_translate(view, v);
+		mat4 projection = GLM_MAT4_IDENTITY_INIT;//投影矩阵
+		glm_perspective(glm_rad(fov), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f,projection);
+		
 		// pass them to the shaders (3 different ways)
 		glUniformMatrix4fv(glGetUniformLocation(ID, "projection"), 1, GL_FALSE, projection[0]);
-		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, view[0]);
+		
 		glBindVertexArray(VAO);
 	
 		for (unsigned int i = 0; i < 10; i++)
@@ -298,6 +327,37 @@ void processInput(GLFWwindow *window)
 {
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, true);
+	float cameraSpeed = 2.5 * deltaTime;
+	vec3 cam_speed = { cameraSpeed,cameraSpeed ,cameraSpeed };
+	vec3 temp,temp2;
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+	{ 
+		glm_vec_mulv(cam_speed,cameraFront,temp);
+		glm_vec_add(cameraPos,temp,cameraPos);
+		//cameraPos += cameraSpeed * cameraFront;
+	}
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+	{
+		glm_vec_mulv(cam_speed, cameraFront, temp);
+		glm_vec_sub(cameraPos, temp, cameraPos);
+		//cameraPos -= cameraSpeed * cameraFront;
+	}
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+	{ 
+		glm_vec_cross(cameraFront, cameraUp, temp2);
+		glm_vec_normalize(temp2);
+		glm_vec_mulv(cam_speed, temp2, temp);
+		glm_vec_sub(cameraPos, temp, cameraPos);
+		//cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+	}
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+	{
+		glm_vec_cross(cameraFront, cameraUp, temp2);
+		glm_vec_normalize(temp2);
+		glm_vec_mulv(cam_speed, temp2, temp);
+		glm_vec_add(cameraPos, temp, cameraPos);
+		//cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+	}
 	//if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
 	//{
 	//	mixValue += 0.001f; // change this value accordingly (might be too slow or too fast based on system hardware)
@@ -320,4 +380,47 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 	// make sure the viewport matches the new window dimensions; note that width and 
 	// height will be significantly larger than specified on retina displays.
 	glViewport(0, 0, width, height);
+}
+void mouse_callback(GLFWwindow* window, double xpos, double ypos)
+{
+	if (firstMouse)
+	{
+		lastX = xpos;
+		lastY = ypos;
+		firstMouse = false;
+	}
+
+	float xoffset = xpos - lastX;
+	float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+	lastX = xpos;
+	lastY = ypos;
+
+	float sensitivity = 0.1f; // change this value to your liking
+	xoffset *= sensitivity;
+	yoffset *= sensitivity;
+
+	yaw += xoffset;
+	pitch += yoffset;
+
+	// make sure that when pitch is out of bounds, screen doesn't get flipped
+	if (pitch > 89.0f)
+		pitch = 89.0f;
+	if (pitch < -89.0f)
+		pitch = -89.0f;
+
+	vec3 front{ cos(glm_rad(yaw)) * cos(glm_rad(pitch)),  sin(glm_rad(pitch)),sin(glm_rad(yaw)) * cos(glm_rad(pitch)) };
+	glm_vec_normalize(front);
+	glm_vec_copy(front, cameraFront);
+}
+
+// glfw: whenever the mouse scroll wheel scrolls, this callback is called
+// ----------------------------------------------------------------------
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+	if (fov >= 1.0f && fov <= 45.0f)
+		fov -= yoffset;
+	if (fov <= 1.0f)
+		fov = 1.0f;
+	if (fov >= 45.0f)
+		fov = 45.0f;
 }
